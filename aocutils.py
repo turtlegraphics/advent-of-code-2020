d87058b9c81aa3c98e7e424bf26095f55f7715a2
Bryan Clair <bryan@slu.edu>
2020-12-12 23:48:43 -0600

    Adding Chinese Remainder Theorem to my toolkit.

--- aocutils.py
+++ aocutils.py
@@ -37,6 +37,29 @@ def parse_args():
 
     return args
 
+# CRT from https://rosettacode.org/wiki/Chinese_remainder_theorem#Python      
+def chinese_remainder(n, a):
+    """solves the chinese remainder theorem for x == a (mod n)"""
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+
+    for n_i, a_i in zip(n, a):
+        p = prod / n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+def mul_inv(a, b):
+    """returns the multiplicative inverse of a (mod b)"""
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a / b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
 class Point:
     """
     A 2d point class
