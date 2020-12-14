4bf4d587098568a8ae00f00595e9ea73833dda3f
Bryan Clair <bryan@slu.edu>
2020-12-14 00:32:58 -0600

    Adding regex parsing to template.

--- solve-template.py
+++ solve-template.py
@@ -5,6 +5,7 @@
 # Day --
 #
 import sys
+import re
 sys.path.append("..")
 import aocutils
 
@@ -12,5 +13,8 @@ args = aocutils.parse_args()
 
 inputlines = [x.strip() for x in open(args.file).readlines()]
 
+parser = re.compile(r"name: (\w+) val: (\d+)") # or whatever
+
 for line in inputlines:
     print line
+    name, val = parser.match(line).groups()
